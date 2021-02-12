c8501b8ab2fe5eb6256eeda51374ee7fbb025a62
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2021-02-12 21:28:38 +0300

    Mobx and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <Router>
+      <NavBar />
       <AppRouter />
     </Router>
   )
