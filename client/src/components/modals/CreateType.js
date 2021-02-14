474ee26cc5a0cbb3c7f9221cc0fe7a3a8ea72076
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2021-02-14 12:04:22 +0300

    Made some changes

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import { Form, Button } from 'react-bootstrap'
+import { createType } from '../../http/deviceAPI'
 
 const CreateType = ({show, onHide}) => {
+    const [value, setValue] = useState('')
+
+    const addType = () => {
+        createType({name: value}).then(data => {
+            setValue('')
+            onHide()
+        })
+    }
+
     return (
         <Modal
             show={show}
@@ -17,11 +27,11 @@ const CreateType = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Control placeholder={'Введите название типа'} />
+                    <Form.Control value={value} onChange={e => setValue(e.target.value)} placeholder={'Введите название типа'} />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant={'outline-success'} onClick={onHide}>Добавить</Button>
+                <Button variant={'outline-success'} onClick={addType}>Добавить</Button>
                 <Button variant={'outline-danger'} onClick={onHide}>Закрыть</Button>
             </Modal.Footer>
         </Modal>
